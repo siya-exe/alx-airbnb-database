2ff9907405b7f3c6fc4ae351754076effb435209
siya <siyangwenya13@gmail.com>
2025-07-05 12:00:10 +0000

    Created a bookings and user ID

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,6 @@
-INNER JOIN ["SELECT", "FROM"]
-LEFT JOIN  ["ORDER BY"]
-FULL OUTER JOIN 
+SELECT * Bookings
+INNER JOIN User ON Bookings.id = User.id 
+
+--- INNER JOIN ["SELECT", "FROM"]
+--- LEFT JOIN  ["ORDER BY"]
+--- FULL OUTER JOIN 
