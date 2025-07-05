20419096ecf02f85cdef296066717447a3c3c693
siya <siyangwenya13@gmail.com>
2025-07-05 12:12:39 +0000

    1. Practice Subqueries

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,6 +1,6 @@
 SELECT * Bookings
 INNER JOIN User ON Bookings.id = User.id 
 
---- INNER JOIN ["SELECT", "FROM"]
+--- INNER JOIN SELECT (["SELECT", "FROM"])
 --- LEFT JOIN  ["ORDER BY"]
 --- FULL OUTER JOIN 
